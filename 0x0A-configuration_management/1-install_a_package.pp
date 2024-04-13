79a0ed4cfad271a5b58652e61d1e9b04943ee25f
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-04-13 14:42:34 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#!/usr/bin/pup
 # Installs flask version 2.1.0
 package {'flask':
-  ensure => '2.1.0',
-  provider => 'pip3'
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  required => Package['pip3']
+}
+
+package { 'pip3':
+    ensure => "installed"
 }
