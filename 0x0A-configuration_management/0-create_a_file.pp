e391e37f63f2a9b13142521c7fad8ef3dae43a3b
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-04-13 12:54:31 +0100

    Modify the 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file in /tmp
-$file_path = "/tmp/school"
+$file_path = '/tmp/school'
 
 file { $file_path:
-	content => 'I love Puppet',
-	mode => 0744,
-	owner => 'www-data',
-	group => 'www-data'
+  content => 'I love Puppet',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data'
 }
