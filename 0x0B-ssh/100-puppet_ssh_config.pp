cc5b34cd3a0694aabffa57729bb7ac6f7bb0098a
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-04-16 22:57:56 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,17 @@
 #A puppet manifest for setting up ssh
-$key_path='~/.ssh/school'
-$conf_path='~/.ssh/config'
-$key_dir='~/.ssh'
-$config_content="Host *\n\tUser ubuntu\n\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/school"
+$key_path='/home/ubuntu/.ssh/school'
+$conf_path='/home/ubuntu/.ssh/config'
+$key_dir='/home/ubuntu/.ssh'
+$config_content="Host *\n\tUser ubuntu\n\tPasswordAuthentication no\n\tIdentityFile /home/ubuntu/.ssh/school"
+
+exec { 'ssh-keygen':
+  command => '/usr/bin/ssh-keygen -f school -b 4096 -N betty'
+}
 
 file { $key_dir:
-  ensure => 'directory',
-  mode   => '0700'
+  ensure  => 'directory',
+  mode    => '0700',
+  require => Exec['ssh-keygen']
 }
 
 file { $key_path:
