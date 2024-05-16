f16c65da999f45de91a8a5d1f12ca39f6cf011e0
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-05-16 07:18:28 +0100

    solve mandatory task

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Increase the ulimit from 15 to 4096 which is the hard limit
+
+exec { 'debug-nginx':
+  # Modify the soft limit
+  command => '/bin/sed' -i "s/15/4096" /etc/default/nginx',
+  # Indicate the path where the command will be found
+  path    => '/usr/local/bin/:/bin/',
+}
+
+
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
