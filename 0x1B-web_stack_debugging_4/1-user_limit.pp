4cb4aadd59785a3051794269b76c1ee57d5d8a19
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-05-16 08:26:01 +0100

    Update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Increase the read write limit for holberton
 exec { 'hard-file-limit-increase':
   command => 'sed -i "/^holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => ['/usr/local/bin/', '/bin/']
+  path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'soft-file-limit-increase':
   command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => ['/usr/local/bin/', '/bin/']
+  path    => '/usr/local/bin/:/bin/'
 }
