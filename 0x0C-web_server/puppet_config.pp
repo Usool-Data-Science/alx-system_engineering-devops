e496e53eedc217068843360e7e81da680615a06e
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-08-04 15:34:57 +0100

    update

---
+++ 0x0C-web_server/puppet_config.pp
@@ -0,0 +1,74 @@
+#Server configuration file using puppet
+
+$root_path = '/var/www/html'
+$root_content = 'Hello World!'
+$default_path = '/etc/nginx/sites-available/default'
+$firewall_content = '[CustomService]\ntitle=My customer firewall\ndescription=Open firewall access to the following ports\nports=80,443,3306,22/tcp'
+$server_content = "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /var/www/html;\n\tindex.html;\n}\n}"
+
+# Update the application package manager
+exec { 'update':
+  command => '/usr/bin/apt-get update'
+}
+
+# Open firewall to some ports
+file { '/etc/ufw/applications.d/web_services':
+  ensure  => 'present',
+  content => $firewall_content
+}
+exec { 'ufw-allow':
+  command => '/usr/bin/sudo ufw allow CustomServices',
+  require => File['/etc/ufw/applications.d/web_services']
+}
+exec { 'ufw-reload':
+  command => 'usr/bin/sudo service ufw reload',
+  require => Exec['ufw-allow']
+}
+
+####### DATABASE SETUP #################
+# Install mysql server
+#package { 'mysql-server':
+#  ensure  => 'installed',
+#  require => Exec['update']
+#}
+# Start mysql server
+#exec {
+#  command => '/usr/bin/sudo systemctl start mysql.service',
+#  require => Package['mysql-server']
+#}
+####### WEBSERVER SETUP ################
+
+# Install nginx
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update']
+}
+
+# Create the root directory
+file { $root_path:
+  ensure => 'directory',
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0755'
+}
+
+# Create the root html file.
+file { "${root_path}/index.html":
+  ensure  => 'present',
+  content => $root_content,
+  notify  => Service['nginx'],
+  require => File[$root_path]
+}
+
+# Configure ngixn
+file { $default_path:
+  ensure  => 'present',
+  content => $server_content,
+  notify  => Service['nginx']
+}
+
+# Restart nginx
+service { 'nginx':
+  ensure => 'running',
+  enable => 'true'
+}
